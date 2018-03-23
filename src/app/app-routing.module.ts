9cf81b57322ee4ee909e6cc23f9b1b149078afb7
wj-soft <wooju_lee@daum.net>
2018-03-23 11:31:19 +0900

    container main-nav recent-content basic css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { ContainerComponent } from './container/container.component';
+import { MainNavComponent } from './main-nav/main-nav.component'
+import { RecentContentComponent } from './recent-content/recent-content.component';
+const routes: Routes = [
+  { path: '', component: RecentContentComponent },
+  { path: 'mainnav', component: MainNavComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { AlertModule } from 'ngx-bootstrap';
+import { ContainerComponent } from './container/container.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { RecentContentComponent } from './recent-content/recent-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    MainNavComponent,
+    RecentContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
